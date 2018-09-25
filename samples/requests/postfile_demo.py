3cf7017c2b4dc73ac4629b16b1b87c883dd416a7
BlessKingslayer <836432552@qq.com>
2018-09-25 14:27:25 +0800

    url去除最后的/

--- samples/requests/postfile_demo.py
+++ samples/requests/postfile_demo.py
@@ -1,5 +1,5 @@
 import requests
 
 files = {'file': open('github_favicon.ico', 'rb')}
-r = requests.post('http://httpbin.org/post/', files=files)
+r = requests.post('http://httpbin.org/post', files=files)
 print(r.text)
