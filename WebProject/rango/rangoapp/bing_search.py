90d758965cb568971f47e297f7959da49b91895f
BlessKingslayer <836432552@qq.com>
2019-01-30 18:12:39 +0800

    aa

--- WebProject/rango/rangoapp/bing_search.py
+++ WebProject/rango/rangoapp/bing_search.py
@@ -1,6 +1,8 @@
 import json
 import requests
+import urllib, re
 from urllib import parse
+from bs4 import BeautifulSoup as BS
 
 subscriptionKey = '3284bc328b4e42cdbb7f4b146244f1a7'
 customConfigId = '11f16227-8322-496c-a278-22259435252d'
@@ -25,7 +27,39 @@ def run_query(search_terms, count=10):
         resobj = json.loads(r.text)
         return resobj['webPages']['value']
     except Exception as e:
-        print(e.value)
+        print(e)
         return []
 
-# run_query('湖人')
+
+def run_query2(search_terms):
+    url = 'https://cn.bing.com/search?'
+    parms = {
+        'q': search_terms,
+        'go': '搜索',
+        'qs': 'ds',
+        'form': 'QBRE'
+    }
+    url = url + parse.urlencode(parms)
+    # r = requests.get(url)
+    try:
+        html = urllib.request.urlopen(url)
+    except urllib.error.HTTPError as e:
+        print(e.code)
+    except urllib.error.URLError as e:
+        print(e.reason)
+
+    soup = BS(html, "html.parser")
+    td = soup.findAll("h2")
+    count = soup.findAll(class_="sb_count")
+    for c in count:
+        print(c.get_text())
+
+    for t in td:
+        print(t.get_text())
+        pattern = re.compile(r'href="([^"]*)"')
+        h = re.search(pattern,str(t))
+        if h:
+            for x in h.groups():
+                print(x)
+
+run_query2('湖人')
